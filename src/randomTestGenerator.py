2ae221e084417309f4a76e9b8705db791c4d07c2
Greg4cr <greg@greggay.com>
2021-06-09 15:31:31 +0200

    Updates to random generator

--- src/randomTestGenerator.py
+++ src/randomTestGenerator.py
@@ -4,49 +4,134 @@ from xml.dom import minidom
 import random
 import string
 
-def selectAction(x):
-    if x==1:
-        f.write("\ta.gender = \'%d\' \n" % random.randint(-1,2))
-    if x==2:
-        f.write("\ta.height = %d.0 \n" % random.randint(-99,999))
-    if x==3:
-        f.write("\ta.weight = %d.0 \n" % random.randint(-99,999))
-    if x==4:
-        f.write("\ta.age = %d \n" % random.randint(-99,999))
-    if x==5:
-        f.write("\ta.calculateBMI(a.height, a.weight)\n")
-    if x==6:
-        f.write("\ta.classifyBMI_teensAndChildren()\n")
-    if x==7:
-        f.write("\ta.classifyBMI_adults()\n")
-        
-        
-#CREATE AND SAVE A TEST CASE PYTHON FILE
-f= open("test_simpleBMI.py","w+") #overwrites the old file with this name
+# TODO - split reusable methods into a helper functions file
 
-f.write("import simpleBMI\nimport pytest\n\n")
+# TODO - pass through command line
+file = 'simpleBMI'
+cut = 'BMICalc'
 
+# TODO - either scrape actions from file or define and document a generic format and expect the user to define the list of actions
+
+# For each constructor, note number of parameters.
+# All parameters are assumed to be integers
+inits = [4]
+
+# Actions
+# Format is [name of action, type of action (assign, method), number of parameters
+# All parameters are assumed to be integers
+actions = [['gender', 'assign', 1], ['height', 'assign', 1], ['weight', 'assign', 1], ['age', 'assign', 1], ['calculateBMI', 'method', 2], ['classifyBMI_teensAndChildren', 'method', 0], ['classifyBMI_adults', 'method', 0]]
+
+''' Example test case:
+[
+[0, [1]],
+[1, [5]],
+[4, [6,7]],
+[6, []]
+]
+
+Each step is [ index in action list , [ parameter values] ]
+'''
+
+# Generates a constructor call
+# Picks a random constructor and generates random (integer) input for that constructor
+def generateConstructor():
+    which_constructor = random.randint(0, len(inits) - 1)
+    num_parameters = inits[which_constructor]
+    parameters = []
+
+    for parameter in range(num_parameters):
+        parameters.append(random.randint(-99,999))
+   
+    return [-1, parameters]
+   
+# Generate a random action on the CUT
+# Selects a random action, then generates random (integer) input for that action
+def generateAction():
+    which_action = random.randint(0, len(actions) - 1)
+    num_parameters = actions[which_action][2]
+    parameters = []
+
+    for parameter in range(num_parameters):
+        parameters.append(random.randint(-99,999))
+   
+    action = [which_action, parameters]
+
+    return action
+
+# Prints genotype representation to a file (pytest code)
+
+def writeToFile(test_suite):
+    #CREATE AND SAVE A TEST CASE PYTHON FILE
+    outfile = 'test_' + file + '.py'
+    f= open(outfile,"w+") #overwrites the old file with this name
+    f.write('import ' + file + '\nimport pytest\n')
+
+    for test in range(len(test_suite)):
+       test_case = test_suite[test]
+       f.write("\ndef test_%d():\n" % test)
+
+       # Initialize the constructor
+       parameters = test_case[0][1]
+       init_string = '\tcut = ' + file + '.' + cut + '(' + str(parameters[0])
+       for parameter in range(1, len(parameters)):
+           init_string = init_string + ',' + str(parameters[parameter])
+
+       init_string += ')\n'
+
+       f.write(init_string)
+
+       # Print each test step
+
+       for action in range(1, len(test_case)): 
+           name = actions[test_case[action][0]][0]
+           parameters = test_case[action][1]
+           type = actions[test_case[action][0]][1]
+
+           out_string = ''
+
+           if type == 'assign':
+               out_string = '\tcut.' + name + ' = ' + str(parameters[0]) + '\n'
+           elif type == 'method':
+               if parameters != []: 
+                   out_string = '\tcut.' + name + '(' + str(parameters[0]) 
+                   for parameter in range(1, len(parameters)):
+                       out_string = out_string + ',' + str(parameters[parameter])
+                   out_string += ')\n'
+               else:
+                   out_string = '\tcut.' + name + '()\n' 
+
+           f.write(out_string)
+   
+    f.close() #changes are only saved after closed
+
+# Prints genotype to a file (pytest code) and measures code coverage
+
+def getCoverage(test_suite):
+    writeToFile(test_suite)
+    os.system('pytest -v --cov=' + file + ' --cov-report term-missing --cov-report xml')
+    xmldoc = minidom.parse('coverage.xml')
+    tag = xmldoc.getElementsByTagName('coverage') 
+    print (tag[0].attributes['line-rate'].value)
+
+
+######### Generate a random test suite (1-20 tests), each with 1-20 actions
+
+test_suite = []
 nTestsCases = random.randint(1,20)
 
 for i in range(nTestsCases):
-    
-    #INSTANTIATE AN OBJECT
-    f.write("def test_%d():\n" % i)
-    f.write("\tgender = \'%d\' \n" % random.randint(-1,2))
-    f.write("\theight = %d.0 \n" % random.randint(-99,999))
-    f.write("\tweight = %d.0 \n" % random.randint(-99,999))
-    f.write("\tage = %d \n" % random.randint(-99,999))
-    f.write("\ta = simpleBMI.BMICalc(height, weight, age, gender)\n")
-    
+    test_case = []    
+    # Initialize the CUT
+    test_case.append(generateConstructor()) 
+    # Generate actions
     nActions = random.randint(1,20)
-    
     for j in range(nActions):
-        action = random.randint(1,7)
-        selectAction(action)
-    
-    
-f.close() #changes are only saved after closed
-os.system("pytest -v --cov=simpleBMI --cov-report term-missing --cov-report xml")
-xmldoc = minidom.parse('coverage.xml')
-tag = xmldoc.getElementsByTagName('coverage') 
-print (tag[0].attributes['line-rate'].value)
+        test_case.append(generateAction())
+    test_suite.append(test_case)
+
+# Print genotype
+print(test_suite)
+
+# Print to file and measure coverage
+getCoverage(test_suite)
+
