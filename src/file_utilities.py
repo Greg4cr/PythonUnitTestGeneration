c1ad43112e7e412cfab887a24db27eea7b01cac8
Greg4cr <greg@greggay.com>
2021-06-15 09:52:33 +0200

    Refactoring writeToFile into file_utilities

--- src/file_utilities.py
+++ src/file_utilities.py
@@ -14,3 +14,48 @@ def parseMetadata(filename):
     metadata = json.loads(data)
 
     return metadata
+
+# Prints genotype representation to a file (pytest code)
+def writeToFile(metadata, test_suite):
+    outfile = 'test_' + metadata["file"] + '.py'
+    f= open(outfile,"w+") #overwrites the old file with this name
+    f.write('import ' + metadata["file"] + '\nimport pytest\n')
+
+    for test in range(len(test_suite)):
+        test_case = test_suite[test]
+        f.write("\ndef test_%d():\n" % test)
+
+        # Initialize the constructor
+        parameters = test_case[0][1]
+        init_string = '\tcut = ' + metadata["file"] + '.' + metadata["class"] + '(' + str(parameters[0])
+        for parameter in range(1, len(parameters)):
+            init_string = init_string + ',' + str(parameters[parameter])
+
+        init_string += ')\n'
+
+        f.write(init_string)
+
+        # Print each test step
+
+        for action in range(1, len(test_case)):
+            name = metadata["actions"][test_case[action][0]]["name"] 
+            parameters = test_case[action][1]
+            type = metadata["actions"][test_case[action][0]]["type"]
+
+            out_string = ''
+
+            if type == 'assign':
+                out_string = '\tcut.' + name + ' = ' + str(parameters[0]) + '\n'
+            elif type == 'method':
+                if parameters != []: 
+                    out_string = '\tcut.' + name + '(' + str(parameters[0]) 
+                    for parameter in range(1, len(parameters)):
+                        out_string = out_string + ',' + str(parameters[parameter])
+                    out_string += ')\n'
+                else:
+                    out_string = '\tcut.' + name + '()\n' 
+
+            f.write(out_string)
+   
+    f.close() 
+
