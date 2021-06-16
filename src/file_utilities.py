223a38a299fbdbee25b433fff798f4b59975f928
Greg4cr <greg@greggay.com>
2021-06-16 14:06:07 +0200

    Adding 'logging' version of print to file for output coverage

--- src/file_utilities.py
+++ src/file_utilities.py
@@ -60,3 +60,48 @@ def writeToFile(metadata, test_suite):
    
     f.close() 
 
+# Prints genotype representation to a file (pytest code)
+# This version logs the output of functions for the output fitness function
+def writeToFileLogging(metadata, test_suite):
+    outfile = metadata["location"] + "test_" + metadata["file"] + ".py"
+    f= open(outfile,"w+") # Overwrites the old file with this name
+    f.write("import " + metadata["file"] + "\nimport pytest\nimport logging\n")
+
+    for test in range(len(test_suite)):
+        test_case = test_suite[test]
+        f.write("\ndef test_%d():\n" % test)
+
+        # Initialize the constructor
+        parameters = test_case[0][1]
+        init_string = "\tcut = " + metadata["file"] + "." + metadata["class"] + "(" + str(parameters[0])
+        for parameter in range(1, len(parameters)):
+            init_string = init_string + "," + str(parameters[parameter])
+
+        init_string += ")\n"
+
+        f.write(init_string)
+
+        # Print each test step
+
+        for action in range(1, len(test_case)):
+            name = metadata["actions"][test_case[action][0]]["name"] 
+            parameters = test_case[action][1]
+            type = metadata["actions"][test_case[action][0]]["type"]
+
+            out_string = ""
+
+            if type == "assign":
+                out_string = "\tcut." + name + " = " + str(parameters[0]) + "\n"
+            elif type == "method":
+                if parameters != []: 
+                    out_string = "\toutput = cut." + name + "(" + str(parameters[0]) 
+                    for parameter in range(1, len(parameters)):
+                        out_string = out_string + "," + str(parameters[parameter])
+                    out_string += ")\n\tlogging.info(output)\n"
+                else:
+                    out_string = "\toutput = cut." + name + "()\n\tlogging.info(output)\n" 
+
+            f.write(out_string)
+   
+    f.close() 
+
